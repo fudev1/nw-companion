5d36e6004a8aabc49054d694a7f8bd32164aa497
Matteo D.V. <matteo.divita@outlook.be>
2024-09-26 23:11:13 +0200

    update: discord_roles is now filtered by type in memberprofile

--- backend/members/models.py
+++ backend/members/models.py
@@ -66,7 +66,7 @@ class Character(BaseProfile):
 
 class CharacterRole(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='roles')
-    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+    role = models.ForeignKey('Role', limit_choices_to={'type': 'game'} ,on_delete=models.CASCADE)
     is_war_ready = models.BooleanField(default=False)
     gear_score = models.IntegerField()
     headwear = models.ForeignKey('inventory.Item', on_delete=models.SET_NULL, null=True, related_name="headwear_roles")
