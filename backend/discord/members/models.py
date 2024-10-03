91ce7b41d5abc150872e4aa5e76910e56085dc7e
Matteo D.V. <matteo.divita@outlook.be>
2024-10-04 01:02:16 +0200

    discord_bot: intents event members

--- backend/discord/members/models.py
+++ backend/discord/members/models.py
@@ -6,8 +6,10 @@ class MemberProfile(models.Model):
     """ profil d'un membre lié à l'utilisateur Django et au Discord """
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
     discord_id = models.CharField(max_length=50, unique=True)
+    discriminator = models.CharField(max_length=4)
     discord_username = models.CharField(max_length=100)
     discord_roles = models.ManyToManyField('roles.DiscordRole', related_name='discord_roles', blank=True,)
+    avatar_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.discord_username} (User: {self.user.username if self.user else 'Invité'})'
+        return f'{self.discord_username}#{self.discriminator} (User: {self.user.username if self.user else 'Invité'})'
--- backend/discord_bot/bot.py
+++ backend/discord_bot/bot.py
@@ -1,10 +1,12 @@
 import discord
 import os
+import requests
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # intents (nécessaire pour que le bot fonctionne)
 intents = discord.Intents.default()
+intents.members = True # récupére les infos sur les membres
 
 # instance du client Discord
 client = discord.Client(intents=intents)
@@ -13,5 +15,29 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'Connecté en tant que {client.user}')
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member.name} a rejoint le serveur')
+    print(f'ID: {member.id}')
+    print(f'username: {member.name}')
+    print(f'discriminator: {member.discriminator}')
+    print(f'avatar URL: {member.avatar_url}')
+
+#     # user_data = {
+#     #     'discord_id': member.id,
+#     #     'discord_username': member.name,
+#     #     'discriminator': member.discriminator,
+#     #     'avatar_url': str(member.avatar_url),
+#     # }
+
+#     # response = requests.post('http://localhost:8000/api/members', json=user_data)
+
+#     # if response.status_code == 201:
+#     #     print(f"User {member.name} enregistré avec succès")
+#     # print(f"erreur lors de l'enregistrement: {response.status_code}")
+
+
 
 client.run(TOKEN)
