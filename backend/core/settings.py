9070af81a9bc6380c556286a06573b67b98ee7e5
Matteo D.V. <matteo.divita@outlook.be>
2024-10-10 01:07:28 +0200

    discord_bot: added environment variables for bot authentication, added function to obtain JWT token for bot, and modified get_member command to send user info to backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-z&rp*22j8ytmh1!^(id18dz7=-27@ilrz&%mb73=4bk12sp)$-'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-nw-companion", "localhost"]
 
 
 # Application definition
