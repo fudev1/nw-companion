82d6951c6d371df617a3830e0fda38a5a7b29e83
Matteo Di Vita <matteo.divita@outlook.be>
2024-10-24 16:56:50 +0200

    docker: service adminer

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),  
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST'),  
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
