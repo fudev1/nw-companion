48005e3bd27b6f25ca3191ff087b9c4da60b37e3
Matteo D.V. <matteo.divita@outlook.be>
2024-11-21 21:01:26 +0100

    ops: structure project

--- angular_front/src/app/new-world/home/home.component.ts
+++ angular_front/src/app/new-world/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../layout/main-layout/header/header.component';
+import { HeroComponent } from '../../layout/main-layout/hero/hero.component';
+import { FooterComponent } from '../../layout/main-layout/footer/footer.component';
 
 @Component({
   selector: 'app-new-world-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, HeroComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class NwHomeComponent {
 
 }
